f8d36c01ea5810bb95fe3204f9b6ed9b334fa17d
jbenitgu <=>
2024-06-26 10:27:27 -0500

    automation

---
+++ src/test/java/com/nttdata/petstore/CrearUsuarioStepsDefs.java
@@ -0,0 +1,33 @@
+package com.nttdata.petstore;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
+
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class CrearUsuarioStepsDefs {
+
+    @Steps
+    CrearUsuario crearUsuario;
+
+    @When("creo el usuario con username {string}, firstname {string}, lastname {string}")
+    public void crearUsuario(String username, String firstName, String lastName){
+        crearUsuario.crearUsuario(username, firstName, lastName);
+    }
+
+    @Then("el código de respuesta es {int}")
+    public void elCódigoDeRespuestaEs(int statusCode) {
+        restAssuredThat(response -> response.statusCode(statusCode));
+    }
+
+    @And("el type es {string}")
+    public void elTypeEs(String type) {
+        restAssuredThat(response -> response.body("'type'", equalTo(type)));
+        System.out.println("Type: " + SerenityRest.lastResponse().body().path("type").toString());
+        System.out.println(SerenityRest.lastResponse().print());
+    }
+}
