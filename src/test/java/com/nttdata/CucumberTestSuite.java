950f8969bedbc4fabfe78e201c968aec393bf3cc
zmguitarrad <zguitarrad@gmail.com>
2024-12-17 21:33:39 -0500

    Add file automatization API

--- src/test/java/com/nttdata/CucumberTestSuite.java
+++ src/test/java/com/nttdata/CucumberTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "classpath:features",
-        tags = "@pruebaWorld"   //  ==> Definir el @tag  a ejecutar
+        tags = "@CreateAndReadOrderStore"   //  ==> Definir el @tag  a ejecutar
 )
 public class CucumberTestSuite {
 }
--- src/test/java/com/nttdata/steps/OrderStoreStep.java
+++ src/test/java/com/nttdata/steps/OrderStoreStep.java
@@ -1,2 +1,75 @@
-package com.nttdata.steps;public class OrderStoreStep {
+package com.nttdata.steps;
+
+import com.nttdata.model.OrderStore;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class OrderStoreStep {
+
+    private String URL_BASE= "https://petstore.swagger.io/v2";
+    private int codigoRespuesta;
+    private OrderStore respuestaOrder;
+
+    public void crearOrden(int id, int petId, int quantity){
+
+        OrderStore nuevaOrden = new OrderStore(id, petId, quantity);
+
+        codigoRespuesta = given()
+                .baseUri(URL_BASE)
+                .contentType("application/json")
+                .body(nuevaOrden)
+                .when()
+                .post("/store/order")
+                .then()
+                .statusCode(200)
+                .extract()
+                .statusCode();
+
+        respuestaOrder = given()
+                .baseUri(URL_BASE)
+                .when()
+                .get("/store/order/"+id)
+                .as(OrderStore.class);
+
+        System.out.println("ID Creado: " + respuestaOrder.getPetId());
+        System.out.println("PetID Creado: " + respuestaOrder.getPetId());
+        System.out.println("Quantity Creado: " + respuestaOrder.getQuantity());
+
+    }
+
+
+    public void validarCodigoRespuesta(int codigoEsperdo){
+        if(codigoRespuesta != codigoEsperdo){
+            throw new AssertionError("Código esperado: " +codigoEsperdo + "Código Obtenido: " +codigoRespuesta);
+        }
+    }
+
+    public OrderStore obtenerRespuestaOrder(){
+        return respuestaOrder;
+    }
+
+    public void consultarOrderStore(int id){
+        respuestaOrder =given()
+                .baseUri(URL_BASE)
+                .when()
+                .get("/store/order/"+id)
+                .as(OrderStore.class);
+    }
+    private void listarOrderStore(OrderStore orderStore){
+        System.out.println("ID Creado: " + orderStore.getPetId());
+        System.out.println("PetID Creado: " + orderStore.getPetId());
+        System.out.println("Quantity Creado: " + orderStore.getQuantity());
+
+    }
+    public void validarCodigoRespuesta1(int i){
+        assertThat(lastResponse().statusCode(),is(i));
+    }
+    public OrderStore obtenerRespuestasdeOrderStore(){
+        return respuestaOrder;
+    }
+
 }
+
